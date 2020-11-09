e1c27b0883640958b94e573ef339d0a566d7918b
Roy <rongliang.zhou@qq.com>
2020-11-09 17:46:31 +0800

    704. Binary Search

---
+++ AlgorithmPractice/AlgorithmPractice/704. Binary Search.swift
@@ -0,0 +1,30 @@
+//
+//  704. Binary Search.swift
+//  AlgorithmPractice
+//
+//  Created by szxy on 2020/11/9.
+//  Copyright © 2020 szxy. All rights reserved.
+//
+
+import Foundation
+
+class Solution704 {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        guard nums.count > 0 else {
+            return -1
+        }
+        var left = 0, right = nums.count - 1
+        // 不确定条件是 < 或者 <= 时，用单个元素的数组来判断
+        while left <= right {
+            let mid = left + (right - left) / 2
+            if nums[mid] == target {
+                return mid
+            } else if nums[mid] < target {
+                left = mid + 1 // 二分查找的关键是每次去除确定不包含的那一半，利用此思路来决定写代码的时候需要或者不需要 +1
+            } else {
+                right = mid - 1
+            }
+        }
+        return -1
+    }
+}
--- AlgorithmPractice/AlgorithmPractice/main.swift
+++ AlgorithmPractice/AlgorithmPractice/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-//// 7. Reverse Integer
+// 7. Reverse Integer
 //let solution7 = Solution7()
 //print(solution7.reverse(123))
 //print(solution7.reverse(-123))
@@ -21,9 +21,14 @@ import Foundation
 //print(solution11.maxArea(height))
 
 // 13. Roman to Integer
-let solution13 = Solution13()
-print(solution13.romanToInt("III"))
-print(solution13.romanToInt("IV"))
-print(solution13.romanToInt("IX"))
-print(solution13.romanToInt("LVIII"))
-print(solution13.romanToInt("MCMXCIV"))
+//let solution13 = Solution13()
+//print(solution13.romanToInt("III"))
+//print(solution13.romanToInt("IV"))
+//print(solution13.romanToInt("IX"))
+//print(solution13.romanToInt("LVIII"))
+//print(solution13.romanToInt("MCMXCIV"))
+
+// 704. Binary Search
+let solution704 = Solution704()
+print(solution704.search([-1,0,3,5,9,12], 9))
+print(solution704.search([-1,0,3,5,9,12], 2))
